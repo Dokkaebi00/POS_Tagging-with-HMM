3db75a9ed5aa02f20d2d3965eeeb5a51c6c167ea
KnightV <9569076+dxv2k@users.noreply.github.com>
2020-12-04 21:37:18 +0700

    add dataset from Coursera NLP Specs, requirements.txt for python

--- CoreNLP/main.py
+++ CoreNLP/main.py
@@ -1,10 +1,11 @@
+import stanza
+nlp = stanza.Pipeline(lang='en', processors = 'tokenize,mwt,pos')
 
 
+doc = nlp('Barack Obama was born in Hawaii')
+doc2 = nlp('Barack Obama was born in Hawaii')
 
-
-
-
-
+print(*[f'word: {word.text}\tupos: {word.upos}\txpos: {word.xpos}\tfeats: {word.feats if word.feats else "_"}' for sent in doc.sentences for word in sent.words], sep='\n')
 
 
 
